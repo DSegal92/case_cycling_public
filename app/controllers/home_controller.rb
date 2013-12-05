2beb9e7d958139f6197c53e8cbfa2a593f8dda0f
Daniel Segal <DSegal92@gmail.com>
2013-12-05 11:29:11 -0500

    Adding dynamic carousel

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
 	def index
 		@blogs = Blog.find(:all, :order => "id DESC").select { |b| b.internal == false}.first(2)
+		@carousels = Carousel.all
 	end
 end
