42c328ea90768199817dda79136d93fa1f9c225c
Daniel Segal <dsegal92@gmail.com>
2013-12-26 18:00:11 -0500

    Updated mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,10 +1,13 @@
 class ContactMailer < ActionMailer::Base
   default from: "postmaster@casecycling.com"
+
    def newMessage(subject, name, email, contents)  
    	@subject = subject
    	@name = name
    	@email = email
    	@contents = contents 
-    mail(to: "contactcasecycling@gmail.com", :subject => "New Email about " + @subject)
+    mail(:to => "contactcasecycling@gmail.com", 
+    	 :subject => "New Email about " + @subject,
+    	 :reply_to => @email)
   end
 end
