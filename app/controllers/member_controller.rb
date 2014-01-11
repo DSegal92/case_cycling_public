992a9c25961024898f598895e4767e834466b1fb
Daniel Segal <DSegal92@gmail.com>
2014-01-10 19:50:23 -0500

    Fixed string comparison with nil

--- app/controllers/member_controller.rb
+++ app/controllers/member_controller.rb
@@ -1,6 +1,6 @@
 class MemberController < ApplicationController
 	def index
-		@members = Member.where("is_officer" => false).sort_by { |a| (a.name.split("\s").second()) }
+		@members = Member.where(Member.arel_table[:is_officer].not_eq(true))
 		@officers = Member.where("is_officer" => true)
 		@coaches = Coach.all
 		@positions = Position.all
