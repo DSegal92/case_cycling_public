d3f2ccc55a631d3d25cd6cb1479f616422407698
Daniel Segal <dsegal92@gmail.com>
2014-01-20 16:16:55 -0500

    only display verified members

--- app/controllers/member_controller.rb
+++ app/controllers/member_controller.rb
@@ -1,6 +1,6 @@
 class MemberController < ApplicationController
 	def index
-		@members = Member.where(Member.arel_table[:is_officer].not_eq(true))
+		@members = Member.where(Member.arel_table[:is_officer].not_eq(true)).where("is_verified" => true)
 		@officers = Member.where("is_officer" => true)
 		@coaches = Coach.all
 		@positions = Position.all
